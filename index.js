4637fd77db8e1202218b9da4daab4ce4771007b2
soriful15 <sorifulislam.rs10@gmailcomm>
2023-05-19 02:13:05 +0600

    tocollection get data done

--- index.js
+++ index.js
@@ -2,10 +2,75 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 // middleware
 require('dotenv').config()
 app.use(cors());
 app.use(express.json());
+
+
+
+// mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.SECRET_KEY}@cluster0.uwuwq9x.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+
+    const bookingCollection = client.db('toyMarket').collection('toyCollection')
+
+
+ // app get 
+ app.get('/allCollection',async(req,res)=>{
+    const cursor=bookingCollection.find()
+    const result=await cursor.toArray()
+    res.send(result)
+ })
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
